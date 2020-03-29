50484b34f86c503d3656859ef4aed97713b98470
yuanshuo <yush.2009@163.com>
2020-03-29 16:11:33 +0800

    update my config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   version: "7.19.0",
   note: 'apifm-wxapi 组件升级至 3.4.0', // 这个为版本描述，无需修改
-  subDomain: "tz", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
+  subDomain: "bec8880d2440366f5d1ae89169e45cd4", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   goodsDetailSkuShowType: 0, // 0 为点击立即购买按钮后出现规格尺寸、数量的选择； 1为直接在商品详情页面显示规格尺寸、数量的选择，而不弹框
   shopMod: 0, // 0为单店铺版本 ； 1为多店铺版本
